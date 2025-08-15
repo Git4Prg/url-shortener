74340b67764e4fb1c70fa3dfd363736a1384404a
Git4Prg <parigyan.sroy@gmail.com>
2025-08-15 12:10:27 +0530

    project done

---
+++ src/main/java/com/mintshock/shortener/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.mintshock.shortener.service;
+
+import com.mintshock.shortener.models.User;
+import com.mintshock.shortener.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: "+ username));
+        return UserDetailsImpl.build(user);
+    }
+}
---
+++ src/test/java/com/mintshock/shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintshock.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
